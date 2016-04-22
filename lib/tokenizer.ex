a2425ed26e59493c729447ced2417c57daac5b66
Christian Briones <cwbriones@gmail.com>
2016-04-21 22:09:24 -0700

    char lists make our tokenizer a bit more elegant

--- lib/tokenizer.ex
+++ lib/tokenizer.ex
@@ -1,8 +1,8 @@
 defmodule Stache.Tokenizer do
   def tokenize(template) do
     template
-    |> String.codepoints
-    |> chunk_tokens([], :text, "")
+    |> String.to_char_list
+    |> chunk_tokens([], :text, [])
   end
 
   def chunk_tokens([], tokens, :text, acc) do
@@ -10,44 +10,43 @@ defmodule Stache.Tokenizer do
     Enum.reverse(tokens)
   end
   def chunk_tokens([], _tokens, _, _), do: {:error, :eol}
-  def chunk_tokens(["{", "{", "{" | stream], tokens, :text, acc) do
+  def chunk_tokens('{{{' ++ stream, tokens, :text, acc) do
     tokens = add_token(tokens, :text, acc)
-    chunk_tokens(stream, tokens, :triple, "")
+    chunk_tokens(stream, tokens, :triple, [])
   end
-  def chunk_tokens(["}", "}", "}" | stream], tokens, :triple, acc) do
+  def chunk_tokens('}}}' ++ stream, tokens, :triple, acc) do
     # We've found a closing }}} after an open {{{.
     tokens = add_token(tokens, :triple, acc)
-    chunk_tokens(stream, tokens, :text, "")
+    chunk_tokens(stream, tokens, :text, [])
   end
-  def chunk_tokens(["{", "{", s | stream], tokens, :text, acc) when s in ["#", "^", "!", "/", ">"] do
+  def chunk_tokens('{{' ++ [s|stream], tokens, :text, acc) when s in [?#, ?^, ?!, ?/, ?>] do
     tag = case s do
-      "#" -> :begin_section
-      "!" -> :comment
-      "^" -> :begin_inverted
-      "/" -> :end_section
-      ">" -> :partial
+      ?# -> :begin_section
+      ?! -> :comment
+      ?^ -> :begin_inverted
+      ?/ -> :end_section
+      ?> -> :partial
     end
     tokens = add_token(tokens, :text, acc)
-    chunk_tokens(stream, tokens, tag, "")
+    chunk_tokens(stream, tokens, tag, [])
   end
-  def chunk_tokens(["{", "{" | stream], tokens, :text, acc) do
+  def chunk_tokens('{{' ++ stream, tokens, :text, acc) do
     tokens = add_token(tokens, :text, acc)
-    chunk_tokens(stream, tokens, :double, "")
+    chunk_tokens(stream, tokens, :double, [])
   end
-  def chunk_tokens(["}", "}" | stream], tokens, s, acc)
+  def chunk_tokens('}}' ++ stream, tokens, s, acc)
     when s in [:double, :comment, :begin_section, :begin_inverted, :end_section, :partial] do
 
     tokens = add_token(tokens, s, acc)
-    chunk_tokens(stream, tokens, :text, "")
+    chunk_tokens(stream, tokens, :text, [])
   end
   def chunk_tokens([c | stream], tokens, state, acc) do
     # text plain ol' text
-    chunk_tokens(stream, tokens, state, acc <> c)
+    chunk_tokens(stream, tokens, state, [c|acc])
   end
 
-  def add_token(tokens, :text, ""), do: tokens
+  def add_token(tokens, :text, []), do: tokens
   def add_token(tokens, state, acc) do
-    token = %{contents: acc, token: state}
-    [token|tokens]
+    [{state, Enum.reverse(acc)}|tokens]
   end
 end
--- test/tokenizer_test.exs
+++ test/tokenizer_test.exs
@@ -4,53 +4,53 @@ defmodule TokenizerTest do
   import Stache.Tokenizer
 
   test "tokenizing a double stache" do
-    assert tokenize("{{foo}}") == [%{token: :double, contents: "foo"}]
+    assert tokenize("{{foo}}") == [{:double, 'foo'}]
   end
 
   test "tokenizing a triple stache" do
-    assert tokenize("{{{foo}}}") == [%{token: :triple, contents: "foo"}]
+    assert tokenize("{{{foo}}}") == [{:triple, 'foo'}]
   end
 
   test "tokenizing plain text" do
-    assert tokenize("foo") == [%{token: :text, contents: "foo"}]
+    assert tokenize("foo") == [{:text, 'foo'}]
   end
 
   test "tokenizing a simple template" do
     template = "<h1>{{name}}</h1><p>{{description}}<p>" <>
-      "<h2>Here's an example.</h2> <pre>{{{example}}}</pre>"
+      "<h2>Here\'s an example.</h2> <pre>{{{example}}}</pre>"
 
     assert tokenize(template) == [
-      %{token: :text, contents: "<h1>"},
-      %{token: :double, contents: "name"},
-      %{token: :text, contents: "</h1><p>"},
-      %{token: :double, contents: "description"},
-      %{token: :text, contents: "<p><h2>Here's an example.</h2> <pre>"},
-      %{token: :triple, contents: "example"},
-      %{token: :text, contents: "</pre>"}
+      {:text, '<h1>'},
+      {:double, 'name'},
+      {:text, '</h1><p>'},
+      {:double, 'description'},
+      {:text, '<p><h2>Here\'s an example.</h2> <pre>'},
+      {:triple, 'example'},
+      {:text, '</pre>'}
     ]
   end
 
   test "tokeninizing a comment" do
-    assert tokenize("{{! some comment }}") == [%{token: :comment, contents: " some comment "}]
+    assert tokenize("{{! some comment }}") == [{:comment, ' some comment '}]
   end
 
   test "tokeninizing a section" do
     assert tokenize("{{#section}}foo{{/section}}") == [
-      %{token: :begin_section, contents: "section"},
-      %{token: :text, contents: "foo"},
-      %{token: :end_section, contents: "section"}
+      {:begin_section,'section'},
+      {:text, 'foo'},
+      {:end_section, 'section'}
     ]
   end
 
   test "tokeninizing an inverted section" do
     assert tokenize("{{^section}}foo{{/section}}") == [
-      %{token: :begin_inverted, contents: "section"},
-      %{token: :text, contents: "foo"},
-      %{token: :end_section, contents: "section"}
+      {:begin_inverted, 'section'},
+      {:text, 'foo'},
+      {:end_section, 'section'}
     ]
   end
 
   test "tokenizing a partial" do
-    assert tokenize("{{>foo}}") == [%{token: :partial, contents: "foo"}]
+    assert tokenize("{{>foo}}") == [{:partial, 'foo'}]
   end
 end

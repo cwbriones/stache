db18ac7a29702852a56231779672c7439fa7959c
Christian Briones <cwbriones@gmail.com>
2016-05-05 21:50:30 -0700

    version 0.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Stache.Mixfile do
 
   def project do
     [app: :stache,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.2",
      deps: deps,
      description: description,
